f5177d1f38f3d78e28bf03e3e46e15f3c0833d24
Chris Hilsdon <chrish@xigen.co.uk>
2020-06-11 22:22:36 +0100

    Improvements to NewMediaFile modal component

--- src/components/NewMediaFile.vue
+++ src/components/NewMediaFile.vue
@@ -1,18 +1,25 @@
 <template>
-    <div class="import">
-      <b-button v-b-modal.modal-1>New Import</b-button>
+  <div class="import">
+    <b-button v-b-modal.import>New Import</b-button>
 
-      <b-modal id="modal-1" title="BootstrapVue">
-          <b-form @submit="onSubmit">
-            <b-form-input v-model="url" placeholder="Enter link to import"></b-form-input>
-            <b-button type="submit" variant="primary">Submit</b-button>
-          </b-form>
-      </b-modal>
-    </div>
+    <b-modal v-model="modalShow" id="import" title="Import new media" hide-footer>
+      <b-form @submit="onSubmit">
+        <b-container class="import-modal">
+          <b-form-row>
+            <b-col cols="10">
+              <b-form-input v-model="url" placeholder="Enter link to import" autofocus></b-form-input>
+            </b-col>
+              <b-col cols="1">
+                <b-button type="submit" variant="primary">Submit</b-button>
+              </b-col>
+            </b-form-row>
+          </b-container>
+      </b-form>
+    </b-modal>
+  </div>
 </template>
-
 <script lang="ts">
-import { BModal, BButton, BForm, BFormInput } from 'bootstrap-vue';
+import { BModal, BButton, BForm, BFormInput, BContainer, BFormRow, BCol } from 'bootstrap-vue';
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component({
@@ -20,27 +27,45 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
     BModal,
     BButton,
     BForm,
-    BFormInput
+    BFormInput,
+    BContainer,
+    BFormRow,
+    BCol
   },
 })
 export default class NewMediaFile extends Vue {
-  @Prop() readonly url!: string;
+  @Prop() url!: string;
+  modalShow!: boolean;
+
+  // eslint-disable-next-line
   onSubmit(evt: any): void {
     evt.preventDefault();
 
     const formData = new FormData();
     formData.append('url', this.url);
 
-    fetch('/import-form', {
-        body: formData,
-        method: 'POST',
-        credentials: 'same-origin'
+    this.url = '';
+    this.modalShow = false;
+
+    fetch(process.env.VUE_APP_BASE_URL + '/import-form', {
+      body: formData,
+      method: 'POST',
+      credentials: 'same-origin'
     }).then(response =>
         response.json().then(json => {
           return json;
         })
     );
   }
+  private data(): object {
+    return {
+        modalShow: false,
+    };
+  }
 }
-
 </script>
+<style>
+.import-modal {
+  margin-top: 20px;
+}
+</style>
