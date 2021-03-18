6fc08b9fee3fe0b3ad1224789efcc450ba657d8b
Chris Hilsdon <chrish@xigen.co.uk>
2021-03-18 00:29:10 +0000

    Updated title after fetching metadata from webapp

--- src/components/MediaPlayer.vue
+++ src/components/MediaPlayer.vue
@@ -15,10 +15,17 @@ import { Component, Watch, Vue } from 'vue-property-decorator';
 })
 export default class MediaPlayer extends Vue {
   playing!: string;
+  uuid!: string | null;
+  metadata: object;
 
   @Watch('playing')
   onPropertyChanged(value: string, oldValue: string) {
       console.log('Player playing changed ' + value + ' | ' + oldValue);
+  }
+
+  @Watch('metadata')
+  onPropertyChangedOne(value: any, oldValue: object) {
+      document.title = value.title;
   }
 
   private data(): object {
@@ -28,12 +35,30 @@ export default class MediaPlayer extends Vue {
   }
 
   private streamMediaStart(data: any): void {
+    console.log(data);
     this.playing = data.stream;
+    this.uuid = data.uuid;
+    this.loadTrack();
+  }
+
+  private loadTrack(): void {
+    console.log(this.uuid);
+    fetch(process.env.VUE_APP_BASE_URL + '/media-file/metadata/' + this.uuid, {
+        method: 'GET',
+        credentials: 'same-origin',
+    }).then((response) => {
+        return response.json();
+    }).then((json) => {
+        this.metadata = json;
+    });
   }
 
   constructor() {
     super();
     this.playing = '';
+    this.uuid = null;
+    this.metadata = {};
+
     EventBus.$on('stream-media-start', (data) => {
       this.streamMediaStart(data);
 
